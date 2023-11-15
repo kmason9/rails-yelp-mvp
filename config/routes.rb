3ca9da5b4afe32f61a132f7490ea94b1fc9c5715
Kirsty Mason <kirsty_mason9@hotmail.com>
2023-11-15 14:56:10 +0000

    restaurants and reviews rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "restaurants#index"
+
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+
+  # get "/restaurants", to: "restaurants#index"
+  # get "/restaurants/:id", to: "restaurants#show"
+
+  resources :restaurants, except: %i[edit update destroy] do
+    resources :reviews, only: %i[new create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category:"belgian"}
+mon_plaisir =  {name: "Mon Plaisir", address: "56A Shoreditch High St, London E1 6PQ", category:"french"}
+captain_flints = {name: "Captain Flint's", address: "Fore Street, Salcombe", phone_number: "07933 592836", category:"italian"}
+oka = {name: "Oka", address: "Broadway Market, London Fields", phone_number: "07933 56666", category:"japanese"}
+ola_china = {name: "Ola China", address: "Marques de Pombal", category:"chinese"}
+
+[dishoom, mon_plaisir, captain_flints, oka, ola_china].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+puts "Created #{restaurant.name}"
+end
+
+#instead of create can do .new then .save
